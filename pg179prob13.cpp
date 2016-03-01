c5af444c04bea755575d236a73788d77a4e01c24
Quodexx <lborel@gmail.com>
2016-02-29 20:27:32 -0800

    Added files via upload

--- pg179prob13.cpp
+++ pg179prob13.cpp
@@ -1,3 +1,4 @@
+
 //Holy digits Batman! The Riddler is planning his next caper
 // somewhere on Pennsylvania Avenue. In his usual sporting fashion,
 // he has left the address in the form of a puzzle. The address on
@@ -9,6 +10,36 @@
 // Write a program that uses a loop (or loops) to find the address where the
 // Riddler plans to strike.
 
+//for (int thousands = 0; thousands <= 9; ++thousands) {
+// for (int hundreds = 0; hundreds <= 9; ++hundreds) {
+//    …
+//then start adding checks:
+//
+//for (… thousands…) {
+//  for (… hundreds…) {
+//    if (hundreds == thousands) {
+//      continue;
+//    }
+//    …
+//
+//and then modify things to match the rules:
+//
+//for (int ones = 0; ones <= 9; ++ones) {
+//
+//could become:
+//
+//for (int ones = 1; ones <= 9; ones += 2) {
+//ok, let me work with this
+//Dave “DMI” Ingram
+//so that it only generates odd numbers
+//ok ☺
+//and with the "if" check I added, you'd actually have:
+//er
+//it's not immediately obvious to me, so I will need to absorb it
+//Dave “DMI” Ingram
+//you'd actually have deeper checks in deeper loops
+
+
 
 #include <iostream>
 
@@ -16,50 +47,27 @@ using namespace std;
 
 int main()
 {
-    // int sum_of_the_digits = 0;
-    int SUM = 27;
-    int next = 0;
-    int divvy = 0;
     int thousands = 0;
     int hundreds = 0;
     int tens = 0;
     int ones = 0;
-    int digit;
+    //int digit;
 
     cout << "Figure out the address" << endl;
-    for (digit=1; digit <=10; digit++)
-    {
-        tens = digit;
-        cout << "tens = " << tens << endl; //debug
-        thousands = (3*tens);
-        cout << "thousands = " << thousands << endl; //debug
-        divvy = (SUM - thousands);
-        cout << "divvy = " << divvy << endl; // debug
-        //next = digit;
-        do
-        {
-            if ((digit != thousands) & (digit != tens)){ // can't have numbers repeat
+    for (thousands = 0; thousands <=9; ++thousands){
+        for (hundreds = 0; hundreds <=9; ++hundreds){
+            for (tens = 0; tens <=9; ++tens){
+                for (ones = 0; ones <=9; ones +=2){
+                    if (hundreds/3 == tens)
+                        if(thousands+hundreds+tens+ones==27)
+                            if((thousands!=hundreds)&&(hundreds!=tens)&&(tens!=ones)&&(hundreds!=ones))
+                            {
+                                cout << "The Address is " << thousands << hundreds << tens << ones << " Pennsylvania Avenue.\n";
 
-                if ( divvy %2 == 0){
-                    cout << divvy << " is even.\n";
+                            }
                 }
-                else {
-                    cout << divvy << " is odd.\n";
-                    // need to figure out the numbers here
-                    hundreds = (divvy/2); // divvy will not be the num
-                    cout << "hundreds = " << hundreds << endl;
-                    tens = (divvy/2) + 1;
-                    cout << "tens = " << tens << endl;
-                    ones = hundreds +1;
-                    cout << "ones = " << ones << endl;
-                    if (thousands+hundreds+tens+ones==27){
-                        cout << "address is " << thousands << hundreds << tens << ones << endl;
-                    }
-                }// else
-            } else
-               digit++;
-        } while (digit<= 9); // do
-    } // for
+            }
+        }
+    }
     return 0;
 } // main
-

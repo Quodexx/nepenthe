30f223173b6467f803ce5270475a7de11bc683d1
Quodexx <lborel@gmail.com>
2016-02-29 20:29:15 -0800

    Added files via upload

--- pg179prob13.cpp
+++ pg179prob13.cpp
@@ -10,34 +10,7 @@
 // Write a program that uses a loop (or loops) to find the address where the
 // Riddler plans to strike.
 
-//for (int thousands = 0; thousands <= 9; ++thousands) {
-// for (int hundreds = 0; hundreds <= 9; ++hundreds) {
-//    …
-//then start adding checks:
-//
-//for (… thousands…) {
-//  for (… hundreds…) {
-//    if (hundreds == thousands) {
-//      continue;
-//    }
-//    …
-//
-//and then modify things to match the rules:
-//
-//for (int ones = 0; ones <= 9; ++ones) {
-//
-//could become:
-//
-//for (int ones = 1; ones <= 9; ones += 2) {
-//ok, let me work with this
-//Dave “DMI” Ingram
-//so that it only generates odd numbers
-//ok ☺
-//and with the "if" check I added, you'd actually have:
-//er
-//it's not immediately obvious to me, so I will need to absorb it
-//Dave “DMI” Ingram
-//you'd actually have deeper checks in deeper loops
+
 
 
 
